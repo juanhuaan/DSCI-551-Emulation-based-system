a948f5e6df84c4d7b9117edb147aaff5600237f8
juanhuaan <juanhuaa@usc.edu>
2022-11-15 16:45:52 -0800

    check data

---
+++ realData/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
+# Create your models here.
+class City(models.Model):
+    name = models.CharField(max_length = 10, primary_key=True)
+    rank = models.IntegerField(default = -1)
+    population = models.IntegerField(max_length=20)
+    
+class Restaurants(models.Model):
+    id = models.CharField(max_length = 30, primary_key=True)
+    name = models.ForeignKey(City,on_delete=models.CASCADE)
+    rate = models.IntegerField(default = -1)
+    review_cnt = models.IntegerField(max_length=200)
+    city = models.CharField(max_length=20)
+    latitude = models.FloatField()
+    longitude = models.CharField(max_length=20)
+    categories = ArrayField(
+            models.CharField(max_length=10, blank=True),
+            size=8,
+    )
+
+class User(models.Model):
+    id = models.CharField(max_length = 30, primary_key=True)
+    name = city = models.CharField(max_length=10)
+
+class Rate(models.Model):
+    userId = models.ForeignKey(User,on_delete=models.CASCADE)
+    restId = models.ForeignKey(Restaurants,on_delete=models.CASCADE)
+    score = models.IntegerField(max_length=10)
+
+
+    
+    
+
+
+    
+    
+    
+    
+    
+# name,rating score,review count,city,state,business id,latitude,longitude,categories
