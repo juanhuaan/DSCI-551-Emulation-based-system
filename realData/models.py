3113b11106971f93c1b91438ab779344d73b5a9a
juanhuaan <juanhuaa@usc.edu>
2022-11-15 16:51:36 -0800

    revise table name in model

--- realData/models.py
+++ realData/models.py
@@ -6,6 +6,8 @@ class City(models.Model):
     name = models.CharField(max_length = 10, primary_key=True)
     rank = models.IntegerField(default = -1)
     population = models.IntegerField(max_length=20)
+    class Meta:
+        db_table='City'
     
 class Restaurants(models.Model):
     id = models.CharField(max_length = 30, primary_key=True)
@@ -19,15 +21,21 @@ class Restaurants(models.Model):
             models.CharField(max_length=10, blank=True),
             size=8,
     )
+    class Meta:
+        db_table='Restaurants'
 
 class User(models.Model):
     id = models.CharField(max_length = 30, primary_key=True)
     name = city = models.CharField(max_length=10)
+    class Meta:
+        db_table='User'
 
 class Rate(models.Model):
     userId = models.ForeignKey(User,on_delete=models.CASCADE)
     restId = models.ForeignKey(Restaurants,on_delete=models.CASCADE)
     score = models.IntegerField(max_length=10)
+    class Meta:
+        db_table='Rate'
 
 
     
