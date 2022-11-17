2cbd6e6b83f3069d1c8c2709bc9f961a99c9458b
XinrongHan <xinrong.work@gmail.com>
2022-11-16 16:05:45 -0800

    firebase data updated

--- fireapp/views.py
+++ fireapp/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import pyrebase
+import csv
+import json
+import pandas as pd
 
 # Create your views here.
 
@@ -20,9 +23,36 @@ firebase = pyrebase.initialize_app(Config)
 authe = firebase.auth()
 database = firebase.database()
 
+# initialize the data to store json objects
+review_data = {}
+user_data = {}
+population_data = {}
+restaurant_data = {}
 
-def index(request):
-    filename = 'cars'
+# file paths
+review = './dataSet/export_review.csv'
+user = './dataSet/export_user.csv'
+population = './dataSet/population.csv'
+restaurant = './dataSet/searchRest.csv'
+
+
+def make_json(csvFilePath, data, k):  # convert csv to json object
+
+    # Open a csv reader called DictReader
+    with open(csvFilePath, encoding='utf-8') as csvf:
+        csvReader = csv.DictReader(csvf)
+
+        # Convert each row into a dictionary
+        # and add it to data
+        for rows in csvReader:
+
+            # Assuming a column named 'No' to
+            # be the primary key
+            key = rows[k]
+            data[key] = rows
+
+
+def index(request):  # run when brower refreshed
 
     data = {"cars3": 3}
     database.set(data)
@@ -35,6 +65,27 @@ def index(request):
                     "p2": "https://edfs-b732d-default-rtdb.firebaseio.com/data/cars2"}}}}}
     database.set(userdata)
 
+    # upload review data
+    make_json(review, review_data, 'uid')
+    if (database.child("review").get().val() == None):
+        database.child("review").set(review_data)
+
+    # upload user data
+    make_json(user, user_data, 'uid')
+    if (database.child("user").get().val() == None):
+        database.child("user").set(user_data)
+
+    # upload population data
+    make_json(population, population_data, 'Rank')
+    if (database.child("population").get().val() == None):
+        database.child("population").set(population_data)
+
+    # upload restaurant data
+    make_json(restaurant, restaurant_data, 'business id')
+    if (database.child("restaurant").get().val() == None):
+        database.child("restaurant").set(restaurant_data)
+
+    filename = 'cars'
     partition1 = database.child("data").child("cars1").get().key()
     part1_location = database.child('root').child('user').child(
         'John').child('cars').child('p1').get()
@@ -45,5 +96,4 @@ def index(request):
         'partition1': partition1,
         'part1_location': value
     }
-
     return render(request, 'index.html', context)
