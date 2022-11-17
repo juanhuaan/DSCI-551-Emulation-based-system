f7fe5c25d6c3da9b6c16fb875e7f78898e9ccaeb
juanhuaan <juanhuaa@usc.edu>
2022-11-16 16:06:48 -0800

    add dataset

--- realData/models.py
+++ realData/models.py
@@ -3,7 +3,7 @@ from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 class City(models.Model):
-    name = models.CharField(max_length = 10, primary_key=True)
+    name = models.CharField(max_length = 100, primary_key=True)
     rank = models.IntegerField(default = -1)
     population = models.IntegerField()
     class Meta:
